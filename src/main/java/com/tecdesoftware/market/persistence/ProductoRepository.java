b832f5009b43ffff3bee087ab10f8dd692c901e1
ZweeTy23 <josealbertodc2006@gmail.com>
2025-07-02 11:28:46 -0600

    MapStruct implemented

---
+++ src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
@@ -0,0 +1,41 @@
+package com.tecdesoftware.market.persistence;
+
+import com.tecdesoftware.market.persistence.crud.ProductoCrudRepository;
+import com.tecdesoftware.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+    
+
+public class ProductoRepository {
+
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll() {
+
+        return(List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos (int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProductoById(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(Producto producto) {
+        productoCrudRepository.delete(producto);
+    }
+}
